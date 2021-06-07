d0fcf6ff1ec405f38ba100aa92371ce3effbcd34
HarisaranG <harithiru2018@gmail.com>
2021-06-07 22:12:37 +0530

    Updated to es2021 version

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2021,
   },
   extends: [
     "plugin:@typescript-eslint/recommended",
