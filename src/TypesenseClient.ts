babbe190fa3d5614d24ec185dcb37721c0533e7b
HarisaranG <harithiru2018@gmail.com>
2021-06-08 11:56:37 +0530

    Created typesense client class

---
+++ src/TypesenseClient.ts
@@ -0,0 +1,40 @@
+import { Client } from "typesense";
+import { node } from "./interfaces/node";
+import { schema } from "./interfaces/schema";
+
+export class TypesenseClient {
+  private client: Client;
+  constructor(
+    apiKey: string,
+    nodes: node[],
+    nearestNode?: node,
+    connectionTimeout?: number
+  ) {
+    this.client = new Client({
+      nodes: nodes,
+      nearestNode: nearestNode,
+      apiKey: apiKey,
+      connectionTimoutSeconds: connectionTimeout,
+    });
+  }
+
+  async createCollection(collectionName: string): Promise<void> {
+    const autoSchema: schema = {
+      name: collectionName,
+      fields: [{ name: ".*", type: "auto" }],
+    };
+    const result = await this.client.collections().create(autoSchema);
+    console.log(result);
+  }
+
+  async importDocuments(
+    collectionName: string,
+    documents: Record<string, unknown>
+  ): Promise<void> {
+    const result = await this.client
+      .collections(collectionName)
+      .documents()
+      .import(documents, { action: "create" });
+    console.log(result);
+  }
+}
