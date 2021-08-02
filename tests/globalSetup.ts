37bcc9fab7b27e43ee25c6581adb4de2cae5e7c3
Harisaran <harithiru2018@gmail.com>
2021-08-02 12:27:59 +0530

    Push docker-compose up to jest setup and teardown

--- tests/globalSetup.ts
+++ tests/globalSetup.ts
@@ -3,10 +3,10 @@ import { Client } from "typesense";
 import { schema } from "../src/interfaces/schema";
 import { TypesenseClient } from "../src/TypesenseClient";
 import { MongoClient as TestClient } from "../src/MongoClient";
+import { setup as setupDevServer } from "jest-dev-server";
+import { exec } from "child_process";
+import * as util from "util";
 
-module.exports = () => {
-  return;
-};
 export interface book {
   id: string;
   title: string;
@@ -33,4 +33,20 @@ declare global {
       toBeIn(expected: string): CustomMatcherResult;
     }
   }
+}
+
+export default async function globalSetup(): Promise<void> {
+  await setupDevServer([
+    {
+      command: "docker-compose up -d",
+      port: 27017,
+      host: "0.0.0.0",
+      protocol: "tcp",
+      usedPortAction: "ignore",
+      launchTimeout: 120000,
+    },
+  ]);
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const newExec = util.promisify(exec);
+  await newExec('docker exec -i mongo mongo --eval "rs.initiate()"');
 }
